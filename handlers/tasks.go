20c85ea2483088bd8f9314daf2bd42a32f4d79f8
Adrian Mezinski <adrianmezinski@infotech.com>
2019-07-15 15:46:18 -0400

    changed handler methods to be of type Todos, thus letting us have access to all properties of the todo object, and cleans up the code. Also removed 1 instance of debugging output from the code

--- handlers/tasks.go
+++ handlers/tasks.go
@@ -10,50 +10,50 @@ import (
 	"gopkg.in/echo.v3"
 )
 
+//Todos ...
+type Todos struct {
+	Db *gorm.DB
+}
+
 //H ...
 type H map[string]interface{}
 
 //GetTasks ...
-func GetTasks(db *gorm.DB) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		return c.JSON(http.StatusOK, models.GetTasks(db))
-	}
+func (t *Todos) GetTasks(c echo.Context) error {
+	return c.JSON(http.StatusOK, models.GetTasks(t.Db))
 }
 
 //PutTask ...
-func PutTask(db *gorm.DB) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		var task models.Task
+func (t *Todos) PutTask(c echo.Context) error {
 
-		c.Bind(&task)
+	var task models.Task
 
-		id, err := models.PutTask(db, task.Name)
+	c.Bind(&task)
 
-		if err == nil {
-			return c.JSON(http.StatusCreated, H{
-				"created": id,
-			})
-		}
-		return err
+	id, err := models.PutTask(t.Db, task.Name)
+
+	if err == nil {
+		return c.JSON(http.StatusCreated, H{
+			"created": id,
+		})
 	}
+	return err
 }
 
 //DeleteTask ...
-func DeleteTask(db *gorm.DB) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		id, _ := strconv.Atoi(c.Param("id"))
-		_, err := models.DeleteTask(db, id)
+func (t *Todos) DeleteTask(c echo.Context) error {
 
-		if err == nil {
-			return c.JSON(http.StatusOK, H{
-				"delete_id": id,
-				"response":  fmt.Sprintf("%s", err),
-			})
-		}
-		fmt.Println(err)
+	id, _ := strconv.Atoi(c.Param("id"))
+	_, err := models.DeleteTask(t.Db, id)
+
+	if err == nil {
 		return c.JSON(http.StatusOK, H{
 			"delete_id": id,
 			"response":  fmt.Sprintf("%s", err),
 		})
 	}
+	return c.JSON(http.StatusOK, H{
+		"delete_id": id,
+		"response":  fmt.Sprintf("%s", err),
+	})
 }
--- todo.go
+++ todo.go
@@ -12,6 +12,8 @@ import (
 
 func main() {
 
+	var todo handlers.Todos
+
 	db, err := gorm.Open("postgres", "host=0.0.0.0 port=5439 user=postgres dbname=postgres sslmode=disable")
 	if err != nil {
 		panic(err)
@@ -21,12 +23,14 @@ func main() {
 
 	defer db.Close()
 
+	todo.Db = db
+
 	e := echo.New()
 
 	e.File("/", "public/index.html")
-	e.GET("tasks", handlers.GetTasks(db))
-	e.PUT("tasks", handlers.PutTask(db))
-	e.DELETE("/tasks/:id", handlers.DeleteTask(db))
+	e.GET("tasks", todo.GetTasks)
+	e.PUT("tasks", todo.PutTask)
+	e.DELETE("/tasks/:id", todo.DeleteTask)
 
 	e.Logger.Fatal(e.Start(":8000"))
 }
